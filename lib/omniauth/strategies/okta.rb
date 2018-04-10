a082056e24151798129c9c781c8646ff0899a2c6
John Duff <duff.john@gmail.com>
2018-04-10 11:41:40 -0400

    Keep refresh token

--- lib/omniauth/strategies/okta.rb
+++ lib/omniauth/strategies/okta.rb
@@ -51,8 +51,9 @@ module OmniAuth
 
       def access_token
         ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
-          :expires_in => oauth2_access_token.expires_in,
-          :expires_at => oauth2_access_token.expires_at
+          :refresh_token => oauth2_access_token.refresh_token,
+          :expires_in    => oauth2_access_token.expires_in,
+          :expires_at    => oauth2_access_token.expires_at
         })
       end
 
